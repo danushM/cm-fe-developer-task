6f92e96f4a9c6b5fd7fe227a05d1309a9dc8ce2d
danushM <44973420+danushM@users.noreply.github.com>
2021-09-10 07:37:18 +0100

    Added submission files

--- src/views/TableList/DataFetcher.js
+++ src/views/TableList/DataFetcher.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const employeeDataFetcher = async (selectedDepartment) => {
   try {
     //Gathering data from API using GET and Axios.
-    var employeeData = await axios.get(
+    let employeeData = await axios.get(
       `https://randomuser.me/api/?seed=${selectedDepartment}&results=10`
     );
 
